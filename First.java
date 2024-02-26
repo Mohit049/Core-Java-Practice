c306c1a773ecf12581b205f83b769cadf810c3f7
Mohit <mohitjajware2000@gmail.com>
2024-02-26 18:31:36 +0530

    Written first java code in that file

---
+++ First.java
@@ -0,0 +1,15 @@
+// Project is a combination of files and libraries
+
+
+// Jshell is a command line interface for Java
+//  To use it type jshell and to exit the command line type --- /exit
+
+//To clear screen in jshell -- ctrl +l
+
+class First{
+
+
+    public static void main(String  a[]){
+        System.out.println("This is my first java program ");
+    }
+}
