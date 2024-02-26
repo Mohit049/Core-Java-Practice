6721a1ab3a3c333c5924a86bb15d6330cf718c37
Mohit <mohitjajware2000@gmail.com>
2024-02-26 22:51:38 +0530

    First.java file updated

--- First.java
+++ First.java
@@ -6,10 +6,34 @@
 
 //To clear screen in jshell -- ctrl +l
 
-class First{
+class  First 
+{
+
+//Use Tab for better indentation -- first select that code and then hit tab
+            public static void main(String  a[])
+            {
+                //Declaring a variable
+                int num=20;
+                // int has different types like -- byte,short,int ,long
+                //Note -- in case of long you should have L at the end to specify that it is long
+                long loo=52325700000L;
+
+                System.out.println("This is my first Java program ");
+                System.out.println("Integer value : "+num);
+
+                System.out.println("Long value : "+loo);
+                double dee=5.6; //Takes 8 bytes
+                System.out.println("Double : "+dee);
+                float fee=5.6f;  // here it is compulsory to write f at the end of a value so that it tells that this thing is float
+                System.out.println("Float : "+fee);
 
 
-    public static void main(String  a[]){
-        System.out.println("This is my first java program ");
-    }
-}
+                // in java char takes 2 bytes and it follows UNICODE  not ASCII
+                char che='d'; //Chracters are written inside single quotes
+                System.out.println("Character : "+che);
+
+                //boolean here is written as true or false not 0 or 1 , like in other languages
+                boolean bool=true;
+                System.out.println("Boolean value : "+bool);
+            }
+        }
