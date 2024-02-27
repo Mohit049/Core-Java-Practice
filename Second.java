c4997eb245ac51ce209a30a9a5bb55e4f1dcae9f
Mohit <mohitjajware2000@gmail.com>
2024-02-27 23:24:31 +0530

    Second.java file added

---
+++ Second.java
@@ -0,0 +1,21 @@
+class Second  {
+
+    public static void main(String args[]){
+
+byte bee=(byte)1257; //typecasted int to byte
+
+
+byte b=122;
+int a=257;
+b=(byte)a; //explicit conversion --- casting
+System.out.println(b);
+double dx=5.4;
+int x=(int)dx;
+System.out.println(x);
+System.out.println(bee);
+
+
+
+    }
+    
+}
