c7bfed9533e2dffb823b93e267ecb0b504e0b3e0
Mohit <mohitjajware2000@gmail.com>
2024-02-28 23:11:00 +0530

    third file added

---
+++ Third.java
@@ -0,0 +1,40 @@
+ class Third {
+    public static void main(String args[]){
+
+//  assignment operators --- +,-,*,/,%(modulus)
+
+
+int a=10;
+a++; //post increment (first assign then increment)
+
+
+
+        System.out.println(a); 
+        a--;//post decrement
+        System.out.println(a);
+
+        //++a --> pre increment
+        //--a --> pre decrement
+    
+    
+
+        //Relational operator --- <,>,<=,>=,==,!=
+    
+    System.out.println(5>7);  //Relatiional operator always return boolean values i.e true or false in JAVA
+    
+    //Logical operators --- &&(and) ||(or) !(not)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
